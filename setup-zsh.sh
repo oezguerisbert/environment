2068791b0e18485024e154c2782e69e6a2594643
Özgür Isbert <oezguerisbert@gmail.com>
2021-03-04 15:21:26 +0100

    fix: loading zsh before copying .zshrc file

--- setup-zsh.sh
+++ setup-zsh.sh
@@ -1,12 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 echo "Installing Zsh"
 
 sudo apt update && sudo apt upgrade -y
 
 sudo apt install zsh -y
 
+zsh
+
 cp .zshrc ~/.zshrc
 
-sh -c 'curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh'
+bash -c 'curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh'
 
 source .zshrc
+
+chsh -s $(which zsh)
