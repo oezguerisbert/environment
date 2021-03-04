52039e7db475cab9c0cfaee2f5d530a7c22e3cf9
Özgür Isbert <oezguerisbert@gmail.com>
2021-03-04 15:15:52 +0100

    fix: wrong sudo apt command fixed

--- setup-zsh.sh
+++ setup-zsh.sh
@@ -3,11 +3,10 @@ echo "Installing Zsh"
 
 sudo apt update && sudo apt upgrade -y
 
-sudo apt install zsh
-
+sudo apt install zsh -y
 
 cp .zshrc ~/.zshrc
 
 bash -c 'curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh'
 
-source ./.zshrc
+source .zshrc
